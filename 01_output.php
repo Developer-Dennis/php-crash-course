6b4a677ecb91abb8a4d54bf34ce64904ec87669a
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-18 16:19:30 +0300

    learned how to declare a variable in php

---
+++ 01_output.php
@@ -0,0 +1,18 @@
+<?php
+// $name = 'Dennis';
+// $age = 12;
+// $has_kids = true;
+// $cash_on_hands = 20.75;
+
+
+// // echo $name . ' is ' .$age . ' years old '
+// echo "{$name} is {$age} years old"
+
+
+
+
+define('HOST', 'Localhost');
+define('DB_NAME', 'dev_db');
+
+echo HOST;
+?>
