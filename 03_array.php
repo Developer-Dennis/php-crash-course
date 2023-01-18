e6342a2fbb04c00c6160d2fcdc6f109d83c2ca79
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-18 16:23:54 +0300

    created an array

---
+++ 03_array.php
@@ -0,0 +1,26 @@
+<?php
+// $numbers = [1, 44, 55, 22];
+// $fruits = array('apple', 'oranges', 'mangoes');
+
+// var_dump($numbers);
+
+$person = [
+    [
+    'first_name' => 'Dennis',
+    'last_name' => 'Kemboi',
+    'email' => 'denkogei24@gmail.com',
+    ],
+    [
+        'first_name' => 'Brad',
+        'last_name' => 'Kogeii',
+        'email' => 'denniskipkogei11@gmail.com',
+    ],
+    [
+        'first_name' => 'Brad',
+        'last_name' => 'Traversy',
+        'email' => 'Traversybradgmail.com',
+    ],
+];
+echo $person[1]['email']
+
+?>
