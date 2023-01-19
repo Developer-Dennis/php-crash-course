30bcb1d7a75d62fcacbd503a57128b26945a80a7
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-19 12:50:50 +0300

    created a function for registered user

---
+++ 06_functions.php
@@ -0,0 +1,11 @@
+<?php
+    function registerUser($email){
+    echo $email . ' registered';
+    }
+registerUser('Dennis');
+
+// function sum($n1,$n2){
+//     return $n1 + $n2;
+// }
+// echo sum(5, 5);
+// ?>
