ac73a4ea754836f9e8423ea866bcae9773b6a066
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-19 10:28:54 +0300

    added while loop

--- 05_loops.php
+++ 05_loops.php
@@ -1,5 +1,12 @@
 <?php
-    for($x = 0; $x <= 10; $x++){
+    // for($x = 0; $x <= 10; $x++){
+    // echo 'Number' . $x . '<br>';
+    // }
+
+
+$x = 1;
+while($x <= 15){
     echo 'Number' . $x . '<br>';
-    }
+    $x++;
+}
 ?>
