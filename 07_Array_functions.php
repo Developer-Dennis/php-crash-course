445e762e92318a077ff3c80b97b3818b55b51de6
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-24 10:51:14 +0300

    merging arrays

--- 07_Array_functions.php
+++ 07_Array_functions.php
@@ -8,23 +8,35 @@ $fruits = ['apples', 'oranges', 'pear'];
 // search array
 // var_dump(in_array('apples', $fruits));
 
-$fruits[] = 'grape';
- array_push($fruits, 'banana');
-array_unshift($fruits, 'mangoes');
+// $fruits[] = 'grape';
+//  array_push($fruits, 'banana');
+// array_unshift($fruits, 'mangoes');
 
-// remove from the end of array
+// // remove from the end of array
 
-array_pop($fruits);
+// array_pop($fruits);
 
 // remove from the start of array
 
-array_shift($fruits);
+// array_shift($fruits);
 
 // remove specific
-unset($fruits[2]);
+// unset($fruits[2]);
 
-print_r($fruits)
+// split into 2 chunks
+
+// $chunked_array = $array_chunk($fruits, 2);
+
+// print_r($chunked_array);
 
 
+// print_r($fruits)
 
+
+$arr1 = [1,2,3];
+$arr2 = [4,5,6];
+
+$arr3 = array_merge($arr1, $arr2);
+
+print_r($arr3);
 ?>
